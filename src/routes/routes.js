467f3dfc175a5ae5a0e69ea5b6372f22472f761c
Vale Melo <valemelo18@gmail.com>
2023-06-19 17:23:31 -0300

    cambio en endpoint mocking y errorhandler

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import ProductRoutes from './productRoutes.js';
 import CartRoutes from './cartRoutes.js';
 import TicketRoutes from './ticketRoutes.js';
+import { faker } from '@faker-js/faker';
 
 const router = express.Router();
 
@@ -11,21 +12,26 @@ router.use('/carts', CartRoutes);
 router.use('/tickets', TicketRoutes);
 
 // Endpoint para el Mocking de productos
+const faker = require('faker');
+
 router.get('/mockingproducts', (req, res) => {
   const mockedProducts = [];
   for (let i = 0; i < 100; i++) {
-    mockedProducts.push({
-      title: `Product ${i}`,
-      description: `Description ${i}`,
-      code: `Code ${i}`,
-      price: i * 10,
-      status: true,
-      stock: i * 100,
-      category: `Category ${i}`,
-      thumbnail: `Thumbnail ${i}`
-    });
+    const product = {
+      title: faker.commerce.productName(),
+      description: faker.lorem.sentence(),
+      code: faker.string.alphanumeric(),
+      price: faker.commerce.price(),
+      status: faker.datatype.boolean(),
+      stock: faker.string.alphanumeric(),
+      category: faker.commerce.department(),
+      thumbnail: faker.image.url()
+    };
+    mockedProducts.push(product);
   }
   res.json(mockedProducts);
 });
+
+
 
 export default router;
